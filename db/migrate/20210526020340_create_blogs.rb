5d66a7e1b96df77a09ab414f7e47f798b164834e
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-05-25 19:09:09 -0700

    blog has these attributes: author, date, title, content

---
+++ db/migrate/20210526020340_create_blogs.rb
@@ -0,0 +1,12 @@
+class CreateBlogs < ActiveRecord::Migration[6.1]
+  def change
+    create_table :blogs do |t|
+      t.string :author
+      t.date :date
+      t.string :title
+      t.string :content
+
+      t.timestamps
+    end
+  end
+end
