ff32cfbf61a6fc4ef1b4e10ba12db0b9941cfc24
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-05-28 00:37:29 -0700

    developer attributes: name, hourly rate, title, jobs

--- db/migrate/20210528070904_create_developers.rb
+++ db/migrate/20210528070904_create_developers.rb
@@ -1,6 +1,10 @@
 class CreateDevelopers < ActiveRecord::Migration[6.1]
   def change
     create_table :developers do |t|
+      t.string :name
+      t.float :hourly_rate
+      t.string :title
+      t.int :jobs
 
       t.timestamps
     end
