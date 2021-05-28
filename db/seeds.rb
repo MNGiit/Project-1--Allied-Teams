f5c7ab69e6a6889c4a4cff81f5b89b32245681d4
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-05-28 00:43:05 -0700

    create seed for blog posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+blog_1 = Blog.create(author: "Coder", title: "Hello world!", content: "This is the first post ever in the blog.")
+blog_2 = Blog.create(author: "Programmer", title: "Goodbye world!", content: "This is the first post by this author.")
+blog_3 = Blog.create(author: "Programmer", title: "Unique Title Goes Here", content: "This is the second post by this author.")
+blog_4 = Blog.create(author: "Developer", title: "Another Blogger", content: "Third author ever to post.")
+blog_5 = Blog.create(author: "Coder", title: "A new blog entry", content: "This is the fifth blog post.")
