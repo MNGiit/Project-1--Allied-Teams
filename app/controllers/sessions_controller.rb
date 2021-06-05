e73ca70a78ea299a79a8ca6ae98544ceb5d7ca84
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-06-04 22:49:22 -0700

    #new will prepare user if session doesn't exist

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,8 +1,10 @@
 class SessionsController < ApplicationController
   def new
-    # if session[:user_id] doesn't have :user_id
-    # @user = User.new
-    # else redirect_to root_path
+    if !session[:user_id]
+      @user = User.new
+    else
+      redirect_to root_path
+    end
   end
 
   def create
