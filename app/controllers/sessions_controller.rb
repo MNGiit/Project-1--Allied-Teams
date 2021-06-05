c2f618a26a8bd31bc04943dcc140cd79ebe88769
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-06-04 22:47:33 -0700

    add methods for create and destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,15 @@
 class SessionsController < ApplicationController
+  def new
+    # if session[:user_id] doesn't have :user_id
+    # @user = User.new
+    # else redirect_to root_path
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
 end
