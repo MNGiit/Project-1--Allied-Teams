367ee452f2d5fb879c6555700303109f9624f116
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-06-04 22:46:24 -0700

    add routes to log in and out

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   get '/', to: 'static#index'
+
+  #log in/out
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
   resources :blogs, only: [:index, :show]
   resources :developers, only: [:index, :show, :new, :create]
 end
