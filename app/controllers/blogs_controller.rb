5b41c777eb4e7900268541a0b595826ade82575c
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-06-04 23:53:50 -0700

    #new and #create will create a new blog post

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -8,6 +8,14 @@ class BlogsController < ApplicationController
     @blog = Blog.find(params[:id])
   end
 
+  def new
+    @blog = Blog.new
+  end
+
+  def create
+    @blog = Blog.create(blog_params)
+    redirect_to @blog
+  end
 
   private
 
