972a2f806edf9f16dc40e949891bb2a720363585
MNGiit <36828920+MNGiit@users.noreply.github.com>
2021-06-04 22:52:15 -0700

    #create will find user by email, check password, and assign id to session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,13 @@ class SessionsController < ApplicationController
   end
 
   def create
-
+    @user = User.find_by(name: params[:user][:email])
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to @user
+    else
+      redirect_to login_path
+    end
   end
 
   def destroy
